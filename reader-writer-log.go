3653309647a5e0ee300ba266e7bd6b1cd2d7121b
Didzis Gosko <didzis@users.noreply.github.com>
2022-08-09 06:13:42 +0300

    improve read-writer logger

--- reader-writer-log.go
+++ reader-writer-log.go
@@ -15,14 +15,17 @@ func (r *ReadWriterLogger) Read(buff []byte) (n int, err error) {
 
 	n, err = r.inner.Read(buff)
 
-	log.Tracef("connection %s read buffer: %s", r.name, string(buff[:n]))
+	log.Tracef("connection %s read buffer (%d bytes): %s", r.name, n, string(buff[:n]))
 
 	return
 }
 
 func (r *ReadWriterLogger) Write(buff []byte) (n int, err error) {
-	log.Tracef("connection %s write buffer: %s", r.name, string(buff))
-	return r.inner.Write(buff)
+	log.Tracef("connection %s write buffer (%d bytes): %s", r.name, len(buff), string(buff))
+	// return r.inner.Write(buff)
+	n, err = r.inner.Write(buff)
+	log.Tracef("connection %s write completed: %d bytes attempted, %d bytes succeeded", r.name, len(buff), n)
+	return
 }
 
 // func (r *ReadWriterLogger) Close() error {
